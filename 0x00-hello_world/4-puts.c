636b3a53ed2b3165cab04087345661634179b904
Aisosa osaretin <vicaisosa@gmail.com>
2022-12-12 12:02:04 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: o
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
